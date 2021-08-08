f70cb59d2ccc3b3babe39a324523e646e743ef02
Pawel Dabrowski <dabrowskip9@gmail.com>
2021-08-08 02:48:37 +0200

    v0.2.0 - updated to support new structure of wiki

--- GenshinAchievementsSaver.user.js
+++ GenshinAchievementsSaver.user.js
@@ -1,11 +1,35 @@
 // ==UserScript==
 // @name         Genshin Achievements Saver
 // @namespace    https://github.com/paolostyle/GenshinAchievementsSaver
-// @version      0.1.0
+// @version      0.2.0
 // @description  Store Genshin Impact achievements to keep track of hidden ones
 // @author       paolostyle
 // @license      GNU GPLv3
-// @match        https://genshin-impact.fandom.com/wiki/Achievements
+// @match        https://genshin-impact.fandom.com/wiki/Wonders_of_the_World
+// @match        https://genshin-impact.fandom.com/wiki/Memories_of_the_Heart
+// @match        https://genshin-impact.fandom.com/wiki/Mortal_Travails:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Mortal_Travails:_Series_II
+// @match        https://genshin-impact.fandom.com/wiki/The_Art_of_Adventure
+// @match        https://genshin-impact.fandom.com/wiki/The_Hero%27s_Journey
+// @match        https://genshin-impact.fandom.com/wiki/Mondstadt:_The_City_of_Wind_and_Song
+// @match        https://genshin-impact.fandom.com/wiki/Liyue:_The_Harbor_of_Stone_and_Contracts
+// @match        https://genshin-impact.fandom.com/wiki/Elemental_Specialist
+// @match        https://genshin-impact.fandom.com/wiki/Marksmanship
+// @match        https://genshin-impact.fandom.com/wiki/Challenger:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Challenger:_Series_II
+// @match        https://genshin-impact.fandom.com/wiki/Challenger:_Series_III
+// @match        https://genshin-impact.fandom.com/wiki/Challenger:_Series_IV
+// @match        https://genshin-impact.fandom.com/wiki/Domains_and_Spiral_Abyss:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Olah!:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Snezhnaya_Does_Not_Believe_in_Tears:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Stone_Harbor%27s_Nostalgia:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Meetings_in_Outrealm:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/Meetings_in_Outrealm:_Series_II
+// @match        https://genshin-impact.fandom.com/wiki/Visitors_on_the_Icy_Mountain
+// @match        https://genshin-impact.fandom.com/wiki/A_Realm_Beyond:_Series_I
+// @match        https://genshin-impact.fandom.com/wiki/A_Realm_Beyond:_Series_II
+// @match        https://genshin-impact.fandom.com/wiki/A_Realm_Beyond:_Series_III
+// @match        https://genshin-impact.fandom.com/wiki/Inazuma:_The_Islands_of_Thunder_and_Eternity_-_Series_I
 // @updateURL    https://github.com/paolostyle/GenshinAchievementsSaver/raw/master/GenshinAchievementsSaver.user.js
 // @supportURL   https://github.com/paolostyle/GenshinAchievementsSaver/issues
 // @grant        none
@@ -29,22 +53,21 @@
     changeAndStoreState(() => (state.has(id) ? state.delete(id) : state.add(id)));
 
   function displaySummary() {
-    const totalAchievementsCount = [
+    const earnedAchievementsCount = [
       ...document.querySelectorAll('input[type="checkbox"][id^=check]'),
     ].filter((i) => i.checked).length;
-    const totalPrimogemsCount = [...document.querySelectorAll('.gas-owned-primogems')].reduce(
+    const earnedPrimogemsCount = [...document.querySelectorAll('.gas-owned-primogems')].reduce(
       (total, element) => total + Number(element.textContent),
       0
     );
 
-    let summaryNode = document.querySelector('.gas-summary');
-    if (!summaryNode) {
-      const toc = document.querySelector('#toc');
-      summaryNode = document.createElement('p');
-      summaryNode.className = 'gas-summary';
-      toc.parentNode.insertBefore(summaryNode, toc);
-    }
-    summaryNode.textContent = `So far you completed ${totalAchievementsCount} achievements, which granted you ${totalPrimogemsCount} Primogems.`;
+    const tableTotalAchievements = document.querySelector('.pi-data-value[data-source="total"]');
+    const totalAchievementsCount = tableTotalAchievements.textContent.split('/').pop();
+    tableTotalAchievements.textContent = `${earnedAchievementsCount}/${totalAchievementsCount}`;
+
+    const tablePrimogems = document.querySelector('.pi-data-value[data-source="primogems"]');
+    const totalPrimogemsCount = tablePrimogems.textContent.split('/').pop();
+    tablePrimogems.textContent = `${earnedPrimogemsCount}/${totalPrimogemsCount}`;
   }
 
   function displayTableSummary(table) {
@@ -121,34 +144,39 @@
 
   function appendCheckboxes() {
     document.querySelectorAll('.article-table tbody > tr').forEach((row) => {
-      if (row.id) {
-        const primogemsCell = row.querySelector('td:last-child').textContent;
-        const achievementLevels = primogemsCell.split('/').map((_, index) => index + 1);
-
-        const checkboxes = achievementLevels.map((level) => {
-          const checkbox = document.createElement('input');
-          const value = `${row.id}___${level}`;
-          checkbox.setAttribute('type', 'checkbox');
-          checkbox.setAttribute('value', value);
-          checkbox.setAttribute('id', `check_${value}`);
-          checkbox.checked = state.has(value);
-          checkbox.addEventListener('change', () => updateState(value));
-
-          return checkbox;
-        });
-
-        const cell = document.createElement('td');
-        cell.style.textAlign = 'center';
-        cell.append(...checkboxes);
-
-        row.prepend(cell);
+      if (!row.id) {
+        const cellToId = (str) => encodeURIComponent(str.replaceAll(' ', '_')).replaceAll('%', '.');
+        const achievementNameCell = row.firstElementChild;
+        achievementNameCell.setAttribute('id', cellToId(achievementNameCell.textContent));
       }
+
+      const primogemsCell = row.querySelector('td:last-child').textContent;
+      const achievementLevels = primogemsCell.split('/').map((_, index) => index + 1);
+
+      const checkboxes = achievementLevels.map((level) => {
+        const checkbox = document.createElement('input');
+        const value = `${row.id}___${level}`;
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('value', value);
+        checkbox.setAttribute('id', `check_${value}`);
+        checkbox.checked = state.has(value);
+        checkbox.addEventListener('change', () => updateState(value));
+
+        return checkbox;
+      });
+
+      const cell = document.createElement('td');
+      cell.style.textAlign = 'center';
+      cell.append(...checkboxes);
+
+      row.prepend(cell);
     });
   }
 
   function adjustFooters() {
     document.querySelectorAll('.article-table tfoot > tr > th:first-child').forEach((footer) => {
       footer.setAttribute('colspan', Number(footer.getAttribute('colspan')) + 1);
+      footer.style.textAlign = 'left';
     });
   }
 
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Tampermonkey script for saving achievements in local storage
 ## About
 [Genshin Impact](https://genshin.mihoyo.com/en) is a popular multiplatform action video game, which I'm playing quite a lot recently. Like many games nowadays it contains **achievements** and many of them (the ones listed under category Wonders of the World in game) are hidden. However there is an article on [Genshin Impact Wiki](https://genshin-impact.fandom.com/wiki/Achievements), probably one of the more popular resources about the game, which contains the list of achievements available in the game and it's kept up to date by the community.
 
+Some time before 2.0 version of Genshin Impact the Achievements page has been split into separate articles, so you have to visit a category first to use the script. All the categories available on 2021-08-08 (ver. 2.0 of the game) are supported.
+
 Here's where my script comes in - obviously, as it's a Wiki page, it only contains the *list* of the achievements, with the script you can track them on that page. It looks like this:
 
 ![image](https://user-images.githubusercontent.com/8234991/116486950-19b4c880-a88f-11eb-905f-883de656f127.png)
@@ -14,12 +16,12 @@ If the achievement has multiple levels (like e.g. `QUEST CLEAR` one on the scree
 
 **IMPORTANT: You have to select the achievements manually first, it will NOT automagically synchronize your achievements from the game.**
 
-You can also see the total number of achievements and Primogems earned in the last paragraph before the table of contents:
+The script also updates the infobox on the top right side of the article with summary of the achievements:
 
-![image](https://user-images.githubusercontent.com/8234991/116487664-c9d70100-a890-11eb-9692-8d81ec430ce2.png)
+![image](https://user-images.githubusercontent.com/8234991/128617232-5f0f55cb-020d-41ba-9288-c5480936e640.png)
 
 ## Installation
-First, you need to install [Tampermonkey](https://www.tampermonkey.net/) for your browser. After you're done, you should be able to install the extension simply by **[clicking here](https://github.com/paolostyle/GenshinAchievementsSaver/raw/master/GenshinAchievementsSaver.user.js)** or by navigating to **[GenshinAchievementsSaver.user.js](https://github.com/paolostyle/GenshinAchievementsSaver/blob/master/GenshinAchievementsSaver.user.js)** file in this repo and clicking *Raw* button in the top right corner, just above the source code.
+First, you need to install [Tampermonkey](https://www.tampermonkey.net/) for your browser. After you're done, you should be able to install the extension simply by **[clicking here](https://github.com/paolostyle/GenshinAchievementsSaver/raw/master/GenshinAchievementsSaver.user.js)** or by navigating to **[GenshinAchievementsSaver.user.js](https://github.com/paolostyle/GenshinAchievementsSaver/blob/master/GenshinAchievementsSaver.user.js)** file in this repo and clicking *Raw* button in the top right corner, just above the source code. Then go to **[Genshin Impact Wiki Achievements page](https://genshin-impact.fandom.com/wiki/Achievements)**, select a category from the list and you'll be able to manage your achievements there.
 
 I'm not sure about the updates, but I *think* they should be automatic.
 
